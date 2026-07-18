4b18cb60cae8df2621396d65042c1def0549831d
nguyentruongquynhnhu <nguyentruongquynhnhu2@gmail.com>
2026-07-18 13:56:33 +0700

    feat: them chuc nang quan ly admin (dang nhap, xem, them, xoa, mo khoa tai khoan)

--- PRO_CTDL_1.cpp
+++ PRO_CTDL_1.cpp
@@ -174,3 +174,302 @@ void addNode(List &l, Node *p)
         l._pTail = p;
     }
 }
+
+void xemDanhSachTaiKhoan(List l) {
+    system("cls");
+    cout << "\t\t\t  <<>> XEM DANH SACH TAI KHOAN <<>> " << endl;
+    Node* p = l._pHead;
+    int dem = 1;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    while (p != NULL) {
+        SetConsoleTextAttribute(h, 13);
+        cout << "Khach hang thu " << dem << ":" << endl;
+        cout << p->value;
+        cout << "Trang thai tai khoan: \t\t\t";
+        if (p->value.getKhoa() == true) {
+            SetConsoleTextAttribute(h, 12);
+            cout << "Bi khoa" << endl;
+        }
+        if (p->value.getKhoa() == false) {
+            SetConsoleTextAttribute(h, 10);
+            cout << "Dang hoat dong" << endl;
+        }
+        cout << endl;
+        p = p->_pNext;
+        dem++;
+    }
+    if (dem == 1) cout << "Hien tai van chua co khach hang." << endl;
+    cout << endl;
+    system("pause");
+    return;
+}
+
+void themTaiKhoan(List& l) {
+    system("cls");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 10);
+    cout << "\t\t\t  <<>> THEM TAI KHOAN <<>> " << endl;
+    SetConsoleTextAttribute(h, 13);
+    Node* p = l._pHead;
+    string id, name, iso;
+    unsigned long money;
+    User tmp;
+    cout << "Luu y, ID khong duoc trung (nhap exit de quay lai Menu)." << endl;
+    do {
+        SetConsoleTextAttribute(h, 2);
+        cout << "Nhap ID (14 chu so): ";
+        cin >> id;
+        SetConsoleTextAttribute(h, 13);
+        if (id == "exit" || id == "EXIT") {
+            cout << "Quay lai Menu" << endl;
+            system("pause");
+            return;
+        }
+        if (id.size() != 14) cout << "ID can phai co du 14 chu so, vui long nhap lai" << endl;
+    } while (id.size() != 14);
+    while (p != NULL) {
+        if (p->value.getID() == id) {
+            do {
+                cout << "ID nay da ton tai, vui long nhap ID moi!!" << endl;
+                cout << p->value << endl;
+                cout << "Hay lay ID khac." << endl;
+                do {
+                    SetConsoleTextAttribute(h, 2);
+                    cout << "Nhap ID (14 chu so): ";
+                    cin >> id;
+                    SetConsoleTextAttribute(h, 13);
+                    if (id == "exit" || id == "EXIT") {
+                        cout << "Quay lai Menu" << endl;
+                        system("pause");
+                        return;
+                    }
+                    if (id.size() != 14) cout << "ID can phai co du 14 chu so, vui long nhap lai" << endl;
+                } while (id.size() != 14);
+            } while (id == p->value.getID());
+            break;
+        }
+        p = p->_pNext;
+    }
+    cout << "Ma PIN mac dinh cua ban: 123456" << endl;
+
+    auto isValidName = [](const string& name) {
+        for (char c : name) {
+            if (!isalpha(c) && c != ' ') {
+                return false;
+            }
+        }
+        return !name.empty();
+        };
+
+    do {
+        cout << "Ho va ten (chi nhap chu cai): ";
+        cin.ignore();
+        getline(cin, name);
+
+        if (!isValidName(name)) {
+            SetConsoleTextAttribute(h, 12);
+            cout << "Ten khong hop le! Chi duoc nhap chu cai va khoang trang." << endl;
+            SetConsoleTextAttribute(h, 13);
+        }
+    } while (!isValidName(name));
+
+    cout << "So tien tao tai khoan: ";
+    do {
+        cin >> money;
+        if (money < 0) cout << "Tien tao tai khoan khong the la so am!!!" << endl;
+    } while (money < 0);
+
+    cout << "Don vi tien te: ";
+    do {
+        cin >> iso;
+        if (iso.size() != 3) cout << "Vui long nhap lai don vi tien te!!!" << endl;
+    } while (iso.size() != 3);
+
+    User tmp2(id, "123456", name, money, iso);
+    addNode(l, getNode(tmp2));
+    cout << "Tao tai khoan thanh cong! Xin chao " << name << endl;
+    system("pause");
+    return;
+}
+
+void delHead(List& l) {
+    if (l._pHead != NULL) {
+        Node* p = l._pHead;
+        l._pHead = p->_pNext;
+    }
+}
+
+void delTail(List& l) {
+    Node* p = l._pHead;
+    while (p->_pNext->_pNext != NULL) {
+        p = p->_pNext;
+    }
+    p->_pNext = NULL;
+}
+
+void xoaTaiKhoan(List& l) {
+    system("cls");
+    cout << "\t\t\t  <<>> XOA TAI KHOAN <<>> " << endl;
+
+    string id;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 10);
+    cout << "Nhap ID tai khoan can xoa (Nhap exit de quay lai Menu)." << endl;
+    do {
+        SetConsoleTextAttribute(h, 2);
+        cout << "ID: ";
+        cin >> id;
+        SetConsoleTextAttribute(h, 13);
+        if (id == "exit" || id == "EXIT") {
+            cout << "Quay lai Menu" << endl;
+            system("pause");
+            return;
+        }
+        if (id.size() != 14) cout << "ID can phai co du 14 chu so, vui long nhap lai" << endl;
+    } while (id.size() != 14);
+    SetConsoleTextAttribute(h, 13);
+    Node* p = l._pHead;
+
+    while (p->_pNext != NULL) {
+        if (id == p->_pNext->value.getID()) {
+            cout << "Xoa tai khoan thanh cong!" << endl;
+            cout << "Da xoa " << p->_pNext->value.getName() << " ra khoi he thong." << endl;
+            p->_pNext = p->_pNext->_pNext;
+            system("pause");
+            return;
+        }
+        p = p->_pNext;
+    }
+
+    SetConsoleTextAttribute(h, 12);
+    cout << "Xoa tai khoan that bai, ID can xoa khong ton tai!" << endl;
+    system("pause");
+    return;
+}
+
+void moKhoaTaiKhoan(List& l) {
+    system("cls");
+    cout << "\t\t\t  <<>> MO KHOA TAI KHOAN <<>> " << endl;
+    string id;
+    int dem = 1;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    Node* p = l._pHead;
+    SetConsoleTextAttribute(h, 10);
+    cout << "Nhap ID tai khoan can mo khoa (nhap exit de quay lai Menu)." << endl;
+    do {
+        SetConsoleTextAttribute(h, 2);
+        cout << "ID: ";
+        cin >> id;
+        SetConsoleTextAttribute(h, 13);
+        if (id == "exit" || id == "EXIT") {
+            cout << "Quay lai Menu" << endl;
+            system("pause");
+            return;
+        }
+        if (id.size() != 14) cout << "ID can phai co du 14 chu so, vui long nhap lai" << endl;
+    } while (id.size() != 14);
+    SetConsoleTextAttribute(h, 10);
+
+    while (p != NULL) {
+        if (id == p->value.getID()) {
+            if (p->value.getKhoa() == false) {
+                SetConsoleTextAttribute(h, 10);
+                cout << "Tai khoan van dang duoc hoat dong binh thuong." << endl;
+                system("pause");
+                return;
+            }
+            else {
+                p->value.setKhoa(false);
+                cout << "Mo khoa tai khoan thanh cong!" << endl;
+                system("pause");
+                return;
+            }
+        }
+        p = p->_pNext;
+    }
+    SetConsoleTextAttribute(h, 12);
+    cout << "Mo tai khoan that bai, ID can mo khoa khong ton tai!" << endl;
+    system("pause");
+    return;
+}
+
+bool dangNhapAdmin(List l) {
+    string user, pass;
+    char ast = ' ';
+    int aste = 0;
+    int dem = 0;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 10);
+    cout << "\t\t\tDANG NHAP ADMIN" << endl;
+    SetConsoleTextAttribute(h, 2);
+    cout << "User: "; cin >> user;
+    for (int i = 0; i < 6; i++) {
+        if (l.getUseri(i) == user) {
+            do {
+                if (dem == 3) {
+                    SetConsoleTextAttribute(h, 13);
+                    cout << "Ban da nhap sai qua qua nhieu lan, vui long thu lai trong giay lat." << endl;
+                    system("pause");
+                    return false;
+                }
+                pass = "";
+
+                SetConsoleTextAttribute(h, 2);
+                cout << "Pass: ";
+                do {
+                    ast = _getch();
+                    if (ast == 13 || ast == ' ') {
+                        break;
+                    }
+                    if (ast == 8 || ast == 127) {
+                        if (pass.size() != 0) {
+                            cout << "\b \b";
+                            pass.erase(pass.size() - 1);
+                            aste--;
+                        }
+                    }
+                    else {
+                        pass += ast;
+                        cout << "*";
+                        aste++;
+                    }
+                } while (ast != 13 || ast != ' ');
+                cout << endl;
+
+                if (l.getPassi(i) == pass) {
+                    SetConsoleTextAttribute(h, 13);
+                    cout << "Dang nhap Admin thanh cong, xin chao " << user << "." << endl;
+                    system("pause");
+                    return true;
+                }
+
+                if (l.getPassi(i) != pass) {
+                    SetConsoleTextAttribute(h, 13);
+                    cout << "Mat khau nhap sai, vui long nhap lai." << endl;
+                    dem++;
+                }
+
+            } while (l.getPassi(i) != pass);
+        }
+    }
+    SetConsoleTextAttribute(h, 13);
+    cout << "Dang nhap that bai, tai khoan hoac mat khau cua ban nhap sai." << endl;
+    system("pause");
+    return false;
+}
+
+void MenuAdmin() {
+    system("cls");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 3);
+    cout << "************* <<>> WELCOME TO AAAAA BANK <<>> ************" << endl;
+    cout << "|\t\t       [MENU ADMIN]                      |" << endl;
+    cout << "|------------- Vui long chon loai chuc nang -------------|" << endl;
+    cout << "|        \t1. Xem danh sach tai khoan\t\t |" << endl;
+    cout << "|        \t2. Them tai khoan         \t\t |" << endl;
+    cout << "|        \t3. Xoa tai khoan          \t\t |" << endl;
+    cout << "|        \t4. Mo khoa tai khoan      \t\t |" << endl;
+    cout << "|        \t5. Quay lai sanh          \t\t |" << endl;
+    cout << "**********************************************************" << endl;
+}
